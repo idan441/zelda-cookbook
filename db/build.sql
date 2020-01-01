70cd149c6f6f25fa1687c329e64106d9729d63e9
idan secret <idan441@gmail.com>
2020-01-01 16:07:46 +0200

    The DB is working, checked the connection via psql.

--- db/build.sql
+++ db/build.sql
@@ -1,9 +1,12 @@
-create database zelda_cookbook;
-create user postgres with encrypted password 'postgres';
-grant all privileges on database zelda_cookbook to postgres;
-
+CREATE USER postgres with encrypted password 'postgres';
+CREATE DATABASE zelda_cookbook;
+GRANT ALL PRIVILEGES ON DATABASE zelda_cookbook TO postgres;
 /*
 From the instructions - 
 1) Set up a postgres server with a database named "zelda_cookbook"
 2) Make sure that user postgres uses paswwrod postgres and has full priveleges on zelda_cookbook
+
+Great tips on how to build this query - 
+https://medium.com/coding-blocks/creating-user-database-and-adding-access-on-postgresql-8bfcd2f4a91e
+
 */
